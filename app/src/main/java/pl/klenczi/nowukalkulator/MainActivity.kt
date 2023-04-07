fa230d89eec908c6c3904a46bedf3a288b0530e2
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-07 22:40:49 +0200

    Podstawowe Obliczenia i ZamianaJednostek powiazanie z MainActivity.kt

--- app/src/main/java/pl/klenczi/nowukalkulator/MainActivity.kt
+++ app/src/main/java/pl/klenczi/nowukalkulator/MainActivity.kt
@@ -1,11 +1,26 @@
 package pl.klenczi.nowukalkulator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pO = findViewById<Button>(R.id.btnPodstawObl)
+        val zJ = findViewById<Button>(R.id.btnZamJedn)
+
+        pO.setOnClickListener{
+            val intent = Intent(this, PodstawoweObliczenia::class.java)
+            startActivity(intent)
+        }
+
+        zJ.setOnClickListener{
+            val intent = Intent(this, ZamianaJednostek::class.java)
+            startActivity(intent)
+        }
     }
 }
