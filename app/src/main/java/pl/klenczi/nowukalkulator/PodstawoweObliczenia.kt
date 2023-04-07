5a313ba670926bcf6811d3d9c90b37fe8ff33223
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-07 22:58:07 +0200

    powrot do MainActivity

--- app/src/main/java/pl/klenczi/nowukalkulator/PodstawoweObliczenia.kt
+++ app/src/main/java/pl/klenczi/nowukalkulator/PodstawoweObliczenia.kt
@@ -1,11 +1,20 @@
 package pl.klenczi.nowukalkulator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PodstawoweObliczenia : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_podstawowe_obliczenia)
+
+        val powrot = findViewById<Button>(R.id.btnBackFromPO)
+
+        powrot.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
