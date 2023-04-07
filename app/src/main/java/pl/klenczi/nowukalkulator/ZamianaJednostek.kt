8f58518b1b986cf17014ef2e56772fc750d0cc29
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-07 23:04:27 +0200

    zamiana jednostek activity

--- app/src/main/java/pl/klenczi/nowukalkulator/ZamianaJednostek.kt
+++ app/src/main/java/pl/klenczi/nowukalkulator/ZamianaJednostek.kt
@@ -1,11 +1,21 @@
 package pl.klenczi.nowukalkulator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ZamianaJednostek : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_zamiana_jednostek)
+
+        val powrot = findViewById<Button>(R.id.btnBackFromZJ)
+
+        powrot.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
