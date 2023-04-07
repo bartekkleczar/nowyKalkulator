7843e676e07d5a2eaecac6fed2591e5bd0399555
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-07 22:42:39 +0200

    ZamianaJednostek activity

---
+++ app/src/main/java/pl/klenczi/nowukalkulator/ZamianaJednostek.kt
@@ -0,0 +1,11 @@
+package pl.klenczi.nowukalkulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ZamianaJednostek : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_zamiana_jednostek)
+    }
+}
