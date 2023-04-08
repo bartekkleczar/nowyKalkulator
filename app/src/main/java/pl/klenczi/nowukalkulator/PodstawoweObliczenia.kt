f1b7673fd8496719e180b9a45c0aa887209e075b
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-08 14:22:29 +0200

    findViewByID wszystkie przyciski

--- app/src/main/java/pl/klenczi/nowukalkulator/PodstawoweObliczenia.kt
+++ app/src/main/java/pl/klenczi/nowukalkulator/PodstawoweObliczenia.kt
@@ -16,5 +16,30 @@ class PodstawoweObliczenia : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+        val ac = findViewById<Button>(R.id.btnAC)
+        val c = findViewById<Button>(R.id.btnC)
+        val procent = findViewById<Button>(R.id.btnProcent)
+        val dzielenie = findViewById<Button>(R.id.btnDzielenie)
+
+        val n7 = findViewById<Button>(R.id.btn7)
+        val n8 = findViewById<Button>(R.id.btn8)
+        val n9 = findViewById<Button>(R.id.btn9)
+        val mnozenie = findViewById<Button>(R.id.btnMnozenie)
+
+        val n4 = findViewById<Button>(R.id.btn4)
+        val n5 = findViewById<Button>(R.id.btn5)
+        val n6 = findViewById<Button>(R.id.btn6)
+        val minus = findViewById<Button>(R.id.btnMinus)
+
+        val n1 = findViewById<Button>(R.id.btn1)
+        val n2 = findViewById<Button>(R.id.btn2)
+        val n3 = findViewById<Button>(R.id.btn3)
+        val plus = findViewById<Button>(R.id.btnPlus)
+
+        val n0 = findViewById<Button>(R.id.btn0)
+        val rowna = findViewById<Button>(R.id.btnRowna)
+
+
     }
 }
