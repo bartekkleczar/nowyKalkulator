82e445870a135732003ec90198b8752710a30f25
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-09 20:20:42 +0200

    dodanie bindingow

--- app/src/main/java/pl/klenczi/nowukalkulator/ZamianaJednostek.kt
+++ app/src/main/java/pl/klenczi/nowukalkulator/ZamianaJednostek.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import pl.klenczi.nowukalkulator.databinding.ActivityPodstawoweObliczeniaBinding
 
 class ZamianaJednostek : AppCompatActivity() {
+    private lateinit var binding: ActivityPodstawoweObliczeniaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_zamiana_jednostek)
+        binding = ActivityPodstawoweObliczeniaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val back = findViewById<Button>(R.id.btnBackFromZJ)
 
