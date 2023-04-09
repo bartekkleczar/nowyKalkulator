689f75fe662fe1ef5bdb19c67e11e6c20b3a00df
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-09 22:56:42 +0200

    przeliczanie jednostek wagi

--- app/src/main/java/pl/klenczi/nowukalkulator/Kg_ZamianaJednostek.kt
+++ app/src/main/java/pl/klenczi/nowukalkulator/Kg_ZamianaJednostek.kt
@@ -3,12 +3,24 @@ package pl.klenczi.nowukalkulator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 
 class Kg_ZamianaJednostek : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kg_zamiana_jednostek)
+
+        var selectedItemInput = ""
+        var selectedItemOutput = ""
+        var input = findViewById<EditText>(R.id.ptInput)
+        var output = findViewById<TextView>(R.id.tvOutput)
+        var convert = findViewById<Button>(R.id.btnConvert)
 
         val back = findViewById<Button>(R.id.btnBackFromCmZJ)
 
@@ -17,5 +29,42 @@ class Kg_ZamianaJednostek : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val inputJednostka = findViewById<Spinner>(R.id.spInput)
+        val outputJednostka = findViewById<Spinner>(R.id.spOutput)
+
+        inputJednostka.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                selectedItemInput = parent.getItemAtPosition(position).toString()
+                convert
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Kod wykonywany, gdy nie wybrano żadnego elementu spinnera.
+            }
+        }
+
+        outputJednostka.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                selectedItemOutput = parent.getItemAtPosition(position).toString()
+                convert
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Kod wykonywany, gdy nie wybrano żadnego elementu spinnera.
+            }
+        }
+
+        fun convert(sII: String = selectedItemInput, sIO: String = selectedItemOutput) {
+            val iT = input.text.toString().toDoubleOrNull() ?: 0.0
+            val lista = mapOf("mg" to 0.001, "g" to 1.0, "dag" to 10.0, "kg" to 1000.0, "t" to 1000000.0, "lb" to 453.592, "oz" to 28.3495)
+
+            val result = iT * lista[sII]!! / lista[sIO]!!
+
+            output.text = result.toString()
+        }
+
+        convert.setOnClickListener {
+            convert()
+        }
     }
 }
